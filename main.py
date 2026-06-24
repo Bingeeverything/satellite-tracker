c904773a38b59820eb083ec0b1779516d417179b
Bingeeverything <mr.0gamero5@gmail.com>
2026-06-24 19:07:12 +1000

    Phase 1, Milestone 1 Complete, extracted the raw exif metadata, sorted to location and creation, and then converted to a greyscale numpy array for calculations

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import exiftool
-import rawpy
+import tifffile
 from dotenv import load_dotenv
 import os
 
@@ -10,9 +10,16 @@ load_dotenv()
 
 #created a env file to store my exiftool file location
 exiftool_path = os.getenv("EXIFTOOL_PATH", "exiftool")
-dng_path = "images/20260309_064353.dng"
+dng_path = "images/20250611_173841.dng"
 
 with exiftool.ExifToolHelper(executable=exiftool_path) as et:
-    for d in et.get_metadata(dng_path):
+    for d in et.get_tags([dng_path], tags= ["SubSecDateTimeOriginal", "GPSAltitude", "GPSPosition"]): #using gps position because its a composite tag and works better in this case
         for k, v in d.items():
             print(f"Dict: {k} = {v}")
+
+#using tifffile to read the image, and then we open the image and get the mean
+image =  tifffile.imread(dng_path)
+
+#doing an arithmetic mean of the np array, so its now a 2d greyscale array
+greyscale_image =  np.mean(image, axis= (2))
+print(greyscale_image)

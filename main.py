b2599f8c17d9063f32e89e01f2a3c6636c5bcecb
Bingeeverything <mr.0gamero5@gmail.com>
2026-06-29 18:24:56 +1000

    added the open cv satellite streak finder, and changed the binary image of 1s and 0s to unit8 since open cv works with that, and it still stays at 0 or 255 because it was in binary before, and at last using hough probabilistic transformation to get the lines start and end points

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ import exiftool
 import tifffile
 from dotenv import load_dotenv
 import os
+import cv2
 
 #Load variables from the .env file
 load_dotenv()
 
 #created a env file to store my exiftool file location
 exiftool_path = os.getenv("EXIFTOOL_PATH", "exiftool")
-dng_path = "images/20250611_173841.dng"
+dng_path = "images/20251015_034507.dng"
 
 with exiftool.ExifToolHelper(executable=exiftool_path) as et:
     for d in et.get_tags([dng_path], tags= ["SubSecDateTimeOriginal", "GPSAltitude", "GPSPosition"]): #using gps position because its a composite tag and works better in this case
@@ -22,8 +23,29 @@ image =  tifffile.imread(dng_path)
 
 #doing an arithmetic mean of the np array, so its now a 2d greyscale array
 greyscale_image =  np.mean(image, axis= (2))
+print(greyscale_image)
 
-threshold = 100
+"""
+In the following code, i lablelled a threshold of 100, below it is the dark sky, and above it is the stars, the city light pollution and such
+Then initialized a variable "compare" which i used comparison operation to compare to the threshold, which give me the ndarray of True and False
+then i converted the true and false to the int value of it using astype
+and to finally printing it, so now i have a array with just 0s and 1s representing the darkness in the sky.
+"""
+threshold = 65
 comparing = (greyscale_image > threshold)
-binary_image = comparing.astype(int)
-print(binary_image)
+binary_image = (comparing * 255).astype('uint8')
+
+# total_cols = binary_image.shape[1]
+# start_index = total_cols//3
+# cropped_image = binary_image[:, start_index:]
+
+# Detect points that form a line
+lines = cv2.HoughLinesP(binary_image, 1, np.pi/180, 200, minLineLength=255, maxLineGap=4)
+
+# Draw lines on the image
+for line in lines:
+    x1, y1, x2, y2 = line[0]
+    cv2.line(binary_image, (x1, y1), (x2, y2),255, 3)
+# Show result
+cv2.imwrite("ResultImage.jpg", binary_image)
+cv2.waitKey(0)

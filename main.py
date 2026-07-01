55ac3b088287dbfaa6abc511a1c00d25a648faf5
Bingeeverything <mr.0gamero5@gmail.com>
2026-07-01 16:30:54 +1000

    Phase 1: Milestone 3 Done, Generated a list of xy coordinates for satellite streaks from the image, 1. Used the pixel at 99.9th percentile to distribute the data, and clipped it so no data is above 255 as we will convert the 12 bit data to 8bit, 2. using a plain gaussian blur with a big kernel size to compensate for the small phone camera

--- main.py
+++ main.py
@@ -23,29 +23,43 @@ image =  tifffile.imread(dng_path)
 
 #doing an arithmetic mean of the np array, so its now a 2d greyscale array
 greyscale_image =  np.mean(image, axis= (2))
-print(greyscale_image)
 
-"""
-In the following code, i lablelled a threshold of 100, below it is the dark sky, and above it is the stars, the city light pollution and such
-Then initialized a variable "compare" which i used comparison operation to compare to the threshold, which give me the ndarray of True and False
-then i converted the true and false to the int value of it using astype
-and to finally printing it, so now i have a array with just 0s and 1s representing the darkness in the sky.
-"""
-threshold = 65
-comparing = (greyscale_image > threshold)
-binary_image = (comparing * 255).astype('uint8')
+#dividing every pixel from the 99.9th place, so its kind of a median and not an outlier
+max_pixel = np.percentile(greyscale_image, 99.9)
+
+#making every pixel bigger than 255 to be m,ax 255
+clipping = np.clip(greyscale_image, None, max_pixel)
+scaled_image = ((clipping / max_pixel) * 255).astype('uint8')
+print(scaled_image)
+
+#blurring the image, and then subtracting the blur from the original image to leave just the trails
+blurred = cv2.GaussianBlur(scaled_image, (151,151), 0)
+signal_float = cv2.subtract(scaled_image, blurred)
+
+_, thresh_img = cv2.threshold(signal_float, 6, 255, cv2.THRESH_BINARY)
 
 # total_cols = binary_image.shape[1]
 # start_index = total_cols//3
 # cropped_image = binary_image[:, start_index:]
 
 # Detect points that form a line
-lines = cv2.HoughLinesP(binary_image, 1, np.pi/180, 200, minLineLength=255, maxLineGap=4)
+lines = cv2.HoughLinesP(thresh_img, 1, np.pi/180, 150, minLineLength=200, maxLineGap=10)
+
+#coordinates
+coordinates = []
 
 # Draw lines on the image
-for line in lines:
-    x1, y1, x2, y2 = line[0]
-    cv2.line(binary_image, (x1, y1), (x2, y2),255, 3)
-# Show result
-cv2.imwrite("ResultImage.jpg", binary_image)
+if lines is not None:
+    print(f"Success! Found {len(lines)} streaks.")
+    for line in lines:
+        x1, y1, x2, y2 = line.ravel()
+        coordinates.append(((x1, y1), (x2, y2)))
+        # Draw a bold white line (255) on the black background
+        cv2.line(thresh_img, (x1, y1), (x2, y2), 255, 3)
+else:
+    print("Warning: No lines met the length requirement. The sky mask remains clear.")
+
+print(coordinates)
+# Save result as a clean black-and-white binary image
+cv2.imwrite("Result Image.jpg", thresh_img)
 cv2.waitKey(0)
